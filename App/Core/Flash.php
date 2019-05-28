d23b79ff16dc05300224639fff2fbddd19badbf5
Bankole Emmanuel <37006356+mrbarnk@users.noreply.github.com>
2019-05-28 13:56:46 -0700

    Update Flash.php

--- App/Core/Flash.php
+++ App/Core/Flash.php
@@ -1,39 +1,43 @@
 <?php
 
-class Flash {
-  function set( $key, $val ) {
-    if ( ! session_id() ) {
-      session_start();
+class Session {
+  public function __construct($key = '') {
+    // // return 'ss';
+    if (!is_null($key)) {
+        if ($this->has( $key )) {
+          $val = $key;
+          $this->delete( $key );
+          return $this->get( $val );
+        }
     }
-    $_SESSION[ 'mbr_php_flash_msg' ][ $key ] = $val;
-  }
-  function has( $key ) {
-    if ( ! session_id() ) {
-      session_start();
-    }
-    return isset( $_SESSION[ 'mbr_php_flash_msg' ][ $key ] );
-  }
-  function get( $key ) {
-    if ( ! session_id() ) {
-      session_start();
-    }
-    if ( ! $this->has( $key ) ) {
-      return null;
-    }
-    $val = $_SESSION[ 'mbr_php_flash_msg' ][ $key ];
-    unset( $_SESSION[ 'mbr_php_flash_msg' ][ $key ] );
-    return $val;
   }
   function put($key, $val) {
     return $this->set($key, $val);
   }
-  function keep( $key ) {
-    if ( ! session_id() ) {
-      session_start();
+  function start() {
+    session_start();
+  }
+  function set( $key, $val ) {
+    $_SESSION[ $key ] = $val;
+  }
+  function get( $key, $def=null ) {
+    return $this->has( $key ) ? $_SESSION[ $key ] : $def;
+  }
+  function has( $key ) {
+    return isset( $_SESSION[ $key ] );
+  }
+  function delete( $key ) {
+    if ( $this->has( $key ) ) {
+      unset( $_SESSION[ $key ] );
     }
-    if ( ! $this->has( $key ) ) {
-      return null;
-    }
-    return $_SESSION[ 'mbr_php_flash_msg' ][ $key ];
+  }
+  function id( $newID='' ) {
+    return ( !empty( $newID ) ) ? session_id( $newID ) : session_id();
+  }
+  function destroy() {
+    session_destroy();
+  }
+  function flush() {
+    $this->destroy();
   }
 }
